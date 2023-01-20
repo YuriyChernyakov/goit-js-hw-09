ee424a3572893a5e12b535219e1fdb6e74efb05e
YuriyChernyakov <100133751+YuriyChernyakov@users.noreply.github.com>
2023-01-20 12:11:48 +0000

    Deploying to gh-pages from @ YuriyChernyakov/goit-js-hw-09@330d2a0043313eee0896028e685ccc1c0c623be5 🚀

---
+++ 01-color-switcher.3bc55b7a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){return t.disabled=!1,void clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.3bc55b7a.js.map
